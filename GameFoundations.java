4be08db613b40b8cce3839a2ac8bde807562a6a6
harrison246 <morriha@whs.school.nz>
2022-05-11 12:05:50 +1200

    gameOfLife

---
+++ GameFoundations.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class GameFoundations here.
+ *
+ * @author (your name)
+ * @version (v1)
+ */
+import java.util.Scanner;
+public class GameFoundations
+{
+    
+
+    Scanner input = new Scanner (System.in);
+    // This scanner will allow me to take in the users input from the keyboard 
+    boolean gameMode = false; 
+    /**
+     * Constructor for objects of class GameFoundations
+     */
+    public GameFoundations()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    
+}
