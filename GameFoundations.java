e84a351f8d766a54a2962dd7ff4b2a85a7c70cac
harrison246 <morriha@whs.school.nz>
2022-05-30 15:20:20 +1200

    randomizer for grid

--- GameFoundations.java
+++ GameFoundations.java
@@ -5,6 +5,9 @@
  * @version (v1)
  */
 import java.util.Scanner;
+import java.util.Random;
+import java.io.File;
+import java.io.IOException;
 public class GameFoundations
 {
 
@@ -16,7 +19,10 @@ public class GameFoundations
      */
     public GameFoundations()
     {
-        int[][] grid = new int[10][10]; 
+        int row=10;
+        int col=30; //coloum
+        
+        int[][] grid = new int[col][row]; 
 
         int z = 0;
         Scanner keyinput = new Scanner(System.in);
@@ -28,18 +34,15 @@ public class GameFoundations
             +"board 2 type b2 \n"
             +"board 3 type b3 \n");
 
-
-    
-
         
         switch (keyinput.nextLine()){
             case "b1":// this makes a square full of 1s
-            for (int y = 0; y < 10; y++){
-                for (int x = 0; x < 10; x ++){
+            for (int y = 0; y < col; y++){
+                for (int x = 0; x < row; x ++){
 
                     for (int a = -1; a < 2; a ++){
                         for (int b = -1; b < 2; b ++){
-                            if (x + b > -1 && y + a > -1 && x + b <= 10 && y + a <= 10 && (b != 0 || a != 0) ){
+                            if (x + b > -1 && y + a > -1 && x + b <= col && y + a <= row && (b != 0 || a != 0) ){
 
                                 grid [y][x] = 1;
                             } else {
@@ -54,7 +57,7 @@ public class GameFoundations
                 System.out.println (" ");
             }
             break;
-            case "b2": //this makes a sqaure of 1s witha border of 0s
+            case "b2": //this makes a sqaure of 1s with a border of 0s
             for (int y = 0; y < 10; y++){
                 for (int x = 0; x < 10; x ++){
 
@@ -71,14 +74,71 @@ public class GameFoundations
                 System.out.println (" ");
             }
             break;
-            case "b3":
+            case "x":
             int gss = 10; //grid shape size
             //for (i=0,i<gss,i++){\
             //grid[i][i]=1
             //grid[i][gss-1-i]}
             break;
-        }
 
+            case "b3": //random gen
+            
+
+            
+
+            // place random numbers into the array 'mygrid'
+            for (int i=0; i<row; i++){
+                for (int j=0; j<col; j++){
+                    //mygrid[i][j] = rand() 2 this is used for capping the amount of random numbers that can be generated;
+                    Random rand = new Random(); 
+                    int int_random = rand.nextInt(2); 
+                    grid[i][j] = int_random ;
+                }
+            }
+
+            //what the grid displays
+            System.out.println();
+            for (int i=0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (grid[i][j] == 0)
+                        System.out.print("0");
+                    else
+                        System.out.print("1");
+                }
+                System.out.println();
+            }
+            System.out.println();
+            break;
+            case "b5": //if using pre-made file
+                System.out.println("enter the exact name of your custom file (incl .txt) - must be in file directory");
+                File customFile=new File (keyinput.nextLine());
+                try{
+                    Scanner fileRead = new Scanner(customFile);
+                    while(fileRead.hasNextLine()){
+                        //reading the 0's and 1's of custom file
+                        String num = fileRead.nextLine();
+                        System.out.println(num);
+
+                        if (num == "1"){
+                            System.out.println("*");
+                        }else if (num == "0"){
+                            System.out.println(".");
+                        }else{
+                            System.out.println("");
+                        }
+
+                    }
+                }
+                catch(IOException e) {
+                    //in case anything goes wrong
+                    e.printStackTrace();
+                }
+                break;
+
+
+        }
 
         // while(!gameModePick){
         // modeChoice=keyinput.nextLine().toLowerCase();
