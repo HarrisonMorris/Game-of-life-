04a56891b43ca449f23e18e91bd7cd6bbcb488a9
HarrisonMorris <102632477+HarrisonMorris@users.noreply.github.com>
2022-08-01 12:14:53 +1200

    Update GameFoundations.java

--- GameFoundations.java
+++ GameFoundations.java
@@ -8,108 +8,130 @@ import java.util.Scanner;
 import java.util.Random;
 import java.io.File;
 import java.io.IOException;
-public class generations
+public class GameOfLife
 {
-
     Scanner input = new Scanner (System.in);
     // This scanner will allow me to take in the users input from the keyboard 
-    boolean gameMode = false; 
+    
     /**
      * Constructor for objects of class GameFoundations
      */
-    public generations()
+    public GameOfLife()
     {
-
-        System.out.println("Good day to you sir/madam I'm here as your humble guide to this simple game also known as connways game of life. now to let us begin please do the following \n"
-            +"please type run\n");
-        int row = 10;
-        int col = 12;
-        boolean [][] grid = new boolean[row][col];
-        for (int y = 0; y < grid[0].length; y++){
-            for (int x = 0; x < grid.length; x++){
-                grid [x][y] = false;  
-
+        
+        int trueCellNeighbours; 
+        boolean gameRun = true; //this tells the computer to loop the code within the while loop until said otherwise.    
+        int row = 20;
+        int col = 30;
+        boolean [][] oldGen = new boolean[row][col]; //this is the grid that is first displayed on the screen but after that it is the previous version of each new grid
+        System.out.println("Good day to you sir/madam I'm here as your humble guide to this simple game also known as conway's game of life. now to let us begin please do the following \n"
+            +"please type run to run one generation\n" +"please type run until to run specific number of generation\n");
+        //these two loops fill the grid array. the first one fills the array with to make ll of them false/dead cels this gives a blank slate for the secound loop to fill the array in a random order with true/alive cells    
+        for (int printCol = 0; printCol < oldGen[0].length; printCol++){  //grid.length and grid[0].length are the row and col variables of the grid array
+            for (int printRow = 0; printRow < oldGen.length; printRow++){
+                oldGen [printRow][printCol] = false;  
             }
         }
-        grid [row-1][col-1] = true;
-        grid [row-2][col-2] = true;
-        grid [row-1][col-2] = true;
-        //grid.length and grid[0].length are the row and col variables of the grid array
-
-        boolean gameRun = true;
-        int trueCellNeighbours; 
-        printArray(grid);
-
+              for (int printCol = 0; printCol < oldGen[0].length; printCol++){ 
+            for (int printRow = 0; printRow < oldGen.length; printRow++){
+                //Math.random randomly generates numbers within the range of 0 to 1 and this determines how frequently alive/true cells occur .
+                if (Math.random() > 0.75){ 
+                    oldGen[printRow][printCol] = true;
+                }
+            }
+        }  
+        
+        printArray(oldGen);
         Scanner UI = new Scanner(System.in);
         while (gameRun == true){
-
-            switch (UI.nextLine()){
+            switch (UI.nextLine()){//UI stand for user input
                 case "run":
-
-                System.out.println("Please type run to advance one generation");
-                boolean [][] newGen = new boolean[grid.length][grid[0].length]; 
-
-                for (int y = 0; y < grid[0].length; y++){
-                    for (int x = 0; x < grid.length; x++){
-                        //the y and x loops are for the cell to detect the surrounding cells y for the up and down and x for the side to side
-                        trueCellNeighbours = 0;
-                        for (int a = -1; a < 2; a ++){
-                            for (int b = -1; b < 2; b ++){
-
-                                if ( (y + b > -1 && x + a > -1 && y + b <= grid[0].length - 1 && x + a <= grid.length - 1 && (b != 0 || a != 0) )&&
-                                (grid [x+a][y+b] == true) ) {
-                                    
-                                    trueCellNeighbours++;
-                                }
-                                
-
-                            }
-                            
-                        }
-                        if (trueCellNeighbours > 3){
-                                    newGen[x][y] = false;
-                                }else if (trueCellNeighbours  < 2){
-                                    newGen[x][y] = false;
-                                }else if (trueCellNeighbours == 2 && grid[x][y] == true){
-                                    newGen[x][y] = true;
-
-                                }else if(trueCellNeighbours == 3){
-                                    newGen[x][y] = true; 
-
-                                }else{
-                                    newGen[x][y] = false;
-
-                                }
-                         
-
-                    }
-
-                }
-
-                grid = newGen; 
-                printArray(newGen);
+                System.out.println("type \"run\" to advance one generation");
+                oldGen = makeNewGen(oldGen); // this makes a new generation by getting the old one and putting it through the rules of the game to make a grid.
+                printArray(oldGen);     
+                System.out.println("type \"run\" to advance one generation\n"+"please type \"runU\" to run specific number of generation\n");
                 break;
                 case "quit":
                 gameRun = false;
                 break;
+                case "runU":
+                System.out.println("Please type how many times you want the game to run");
+                int runAmountTimes = UI.nextInt();
+                // runs the makeNewGrid method the amount of times dectated by the user. 
+                for (int checkRunAmountTimes = 0; checkRunAmountTimes < runAmountTimes; checkRunAmountTimes++){
+                    oldGen = makeNewGen(oldGen);
+                    printArray(oldGen);
+                }
+                System.out.println("type \"run\" to run one generation\n"+"please type \"runU\" to run specific number of generation\n"+"please type\"changeCell\"to change a specific cell");
+                break;
+                case "changeCell": // this intakes the the X and Y coordinate and makes the cell the opposite state that it currently is. 
+                System.out.println("to change cell you will need to type the x,y coodernate in this order to change the cell");
+                System.out.println("make sure the X coodinate is between 0 and "+(row -1)+" and the Y coodinate is between 0 and "+(col -1));
+                printArray(oldGen);
+                 int cellCoordinateX = UI.nextInt();
+                 int cellCoordinateY = UI.nextInt();                 
+                if (oldGen[cellCoordinateX][cellCoordinateY] == false){
+                    oldGen[cellCoordinateX][cellCoordinateY] = true;
+                    } else {
+                    oldGen[cellCoordinateX][cellCoordinateY] = false;
+                    }
+                printArray(oldGen);
+                break;
             }
         }
-
     }
-
-    public void printArray (boolean [][] grid){
-        for (int x = 0; x < grid.length; x++){
-            for (int y = 0; y < grid[0].length; y++){
-                if(grid [x][y] == true){
-                    System.out.print("O" + ", ");
-                }  else if(grid [x][y] == false){
-                    System.out.print("X" + ", ");
-
+    // this method prints out the current array
+    public void printArray (boolean [][] oldGen){
+        //this checks each cell of the oldGen array and prints out cell depeneding on if its false(a space appears in cell) or true(a O apears in cell). 
+        for (int printCol = 0; printCol < oldGen.length; printCol++){
+            for (int printRow = 0; printRow < oldGen[0].length; printRow++){
+                if(oldGen [printCol][printRow] == true){
+                    System.out.print("O,");
+                }  else if(oldGen [printCol][printRow] == false){
+                    System.out.print(" ,");
                 }
             }
             System.out.println();
         }
         System.out.println("");
     }
-
+    // this method makes the newGen array for the next grid
+    public boolean[][] makeNewGen (boolean [][] oldGen ){
+        boolean [][] newGen = new boolean[oldGen.length][oldGen[0].length];
+        int trueCellNeighbours;
+        // sets newGen grid to all false 
+        for (int printCol = 0; printCol < oldGen[0].length; printCol++){
+            for (int printRow = 0; printRow < oldGen.length; printRow++){
+                newGen [printCol][printRow] = false;  
+            }
+        }
+        // this function is used to make the new generation by going through each cell and checking its neighbours status (true/alive or false/dead) this is done through using cellRow and cellColloum to change the cell we are looking at for its neighbours and 
+        // using the cellCheckLeft and cellCheckRight to check the cells around the selected cell.
+        for (int cellColumn = 0; cellColumn < oldGen[0].length; cellColumn++){
+            for (int cellRow = 0; cellRow < oldGen.length; cellRow++){
+                trueCellNeighbours = 0;
+                for (int cellCheckLeftRight = -1; cellCheckLeftRight < 2; cellCheckLeftRight ++){
+                    for (int cellCheckUpDown = -1; cellCheckUpDown < 2; cellCheckUpDown ++){
+                        if ( (cellColumn + cellCheckUpDown > -1 && cellRow + cellCheckLeftRight > -1 && cellColumn + cellCheckUpDown <= oldGen[0].length - 1 && cellRow + cellCheckLeftRight <= oldGen.length - 1 && (cellCheckUpDown != 0 || cellCheckLeftRight != 0) )&&
+                        (oldGen [cellRow+cellCheckLeftRight][cellColumn+cellCheckUpDown] == true) ) {
+                            trueCellNeighbours++;
+                        }
+                    }
+                }
+                // these are the rules of conways game of life 
+                if (trueCellNeighbours >= 4){
+                    newGen[cellRow][cellColumn] = false;
+                }else if (trueCellNeighbours  <= 1){
+                    newGen[cellRow][cellColumn] = false;
+                }else if (trueCellNeighbours == 2 && oldGen[cellRow][cellColumn] == true){
+                    newGen[cellRow][cellColumn] = true;
+                }else if(trueCellNeighbours == 3){
+                    newGen[cellRow][cellColumn] = true; 
+                }else{
+                    newGen[cellRow][cellColumn] = false;
+                }
+            }
+        }
+        return newGen;
+    }
 }
